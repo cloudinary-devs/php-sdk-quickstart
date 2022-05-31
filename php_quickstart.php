7a55fadb75478e6a9b8b298dcb7d39c889a8dc89
MeirFeinberg <meir.feinberg@cloudinary.com>
2022-05-31 21:36:41 +0300

    Update php_quickstart.php

--- php_quickstart.php
+++ php_quickstart.php
@@ -2,45 +2,43 @@
 
 <head>
     <title>PHP Quick Start</title>
-    <style>
-        body {
-            background-color: aliceblue;
-            color: black;
-        }
-    </style>
 </head>
 
 <body>
 <?php
-echo '<h1>Cloudinary PHP Quick Start</h1>';
 
 require_once __DIR__ . '/vendor/autoload.php';
-    
+
+
 use Cloudinary\Api\Admin\AdminApi;
 use Cloudinary\Api\Upload\UploadApi;
+use Cloudinary\Configuration\Configuration;
 use Cloudinary\Tag\ImageTag;
+use Cloudinary\Transformation\Background;
 use Cloudinary\Transformation\Resize;
 
+echo '<h1>Cloudinary PHP Quick Start</h1>';
+
 // replace `cloudinary://my_key:my_secret@my_cloud_name` with your environment variable
-$cloudinary = new Cloudinary('cloudinary://my_key:my_secret@my_cloud_name?secure=true')
-    
-    
+
+Configuration::instance('cloudinary://my_key:my_secret@my_cloud_name?secure=true');
+
 // Upload API
 echo '<h2>Upload API Response</h2>';
 
-// Upload the image 
+// Upload the image
 $upload = new UploadApi();
 echo '<pre>';
 echo json_encode(
-  $upload->upload('https://res.cloudinary.com/demo/image/upload/flower.jpg', [
-    'public_id' => 'flower_sample', 
-    'use_filename' => TRUE, 
-    'overwrite' => TRUE],
-  JSON_PRETTY_PRINT
+    $upload->upload('https://res.cloudinary.com/demo/image/upload/flower.jpg', [
+        'public_id' => 'flower_sample',
+        'use_filename' => TRUE,
+        'overwrite' => TRUE]),
+    JSON_PRETTY_PRINT
 );
 echo '</pre>';
 
-    
+
 // Admin api
 echo '<h2>Admin API Response</h2>';
 
@@ -48,21 +46,21 @@ echo '<h2>Admin API Response</h2>';
 $admin = new AdminApi();
 echo '<pre>';
 echo json_encode($admin->asset('flower_sample', [
-  'colors' => TRUE]), JSON_PRETTY_PRINT
+    'colors' => TRUE]), JSON_PRETTY_PRINT
 );
 echo '</pre>';
 
-    
+
 // Delivery with transformations
 echo '<h2>Cloudinary Image</h2>';
 
 // Create the image tag with the transformed image
-$imgtag = $cloudinary->imageTag('flower_sample')
-  ->resize(Resize::pad()
-    ->width(400)
-    ->height(400)
-    ->background(Background::predominant())
-  );
+$imgtag = (new ImageTag('flower_sample'))
+    ->resize(Resize::pad()
+        ->width(400)
+        ->height(400)
+        ->background(Background::predominant())
+    );
 
 echo $imgtag;
 // The code above generates the following HTML image tag (for the demo account):
@@ -75,3 +73,4 @@ echo '<br>';
 </body>
 
 </html>
+
